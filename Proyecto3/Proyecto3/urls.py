3a35c015499e4d6f0c8878b09da51e8e70890090
Antonella Tissandie <antonellatissandie@hotmail.com>
2023-05-01 21:31:17 -0300

    seguro

--- Proyecto3/Proyecto3/urls.py
+++ Proyecto3/Proyecto3/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from App1.views import curso
+from django.urls import path,include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('curso/',curso)
+    path('App1/',include('App1.urls'))
 ]
